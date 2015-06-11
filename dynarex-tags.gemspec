c4a09ce7abb8dc52d80a76ca4041e0e96e7f27ac
James Robertson <james@r0bertson.co.uk>
2015-06-11 12:17:34 +0100

    refactoring

--- dynarex-tags.gemspec
+++ dynarex-tags.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
   s.name = 'dynarex-tags'
-  s.version = '0.1.9'
+  s.version = '0.2.0'
   s.summary = 'dynarex-tags'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
-  s.add_dependency('dynarex') 
+  s.add_runtime_dependency('dynarex', '~> 1.5', '>=1.5.23') 
   s.signing_key = '../privatekeys/dynarex-tags.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
   s.email = 'james@r0bertson.co.uk'
   s.homepage = 'https://github.com/jrobertson/dynarex-tags'
+  s.required_ruby_version = '>= 2.1.2'
 end
--- lib/dynarex-tags.rb
+++ lib/dynarex-tags.rb
@@ -7,79 +7,43 @@ require 'fileutils'
 
 class DynarexTags
 
-  def initialize(tags_path)
+  def initialize(tags_parent_path)
 
-    FileUtils.mkdir_p tags_path
-    Dir.chdir tags_path
-    @index_filename = File.join(tags_path, '_dynarextags.xml')
+    FileUtils.mkdir_p File.join(tags_parent_path, 'tags')
+    Dir.chdir File.join(tags_parent_path, 'tags')
+    @index_filename = File.join(tags_parent_path, 'dxtags.xml')
   end
 
   def generate(category_url)
 
     h = {}
-    category_dynarex = Dynarex.new category_url
+    dx = Dynarex.new category_url
 
-    category_dynarex.to_h.each do |item|
+    dx.all.each do |x|
       
-      url, title = item.values[0..1]
-      next unless title.is_a? String
-      
-      fields = title.scan(/#(\w+)/)
-      next if fields.empty?
-      
-      fields.flatten.uniq.each {|tag| save_tag(h, tag, title, url)}
+      x.title.scan(/\B#(\w+)/).map(&:first).uniq\
+                                 .each {|tag| save_tag(h, tag, x.title, x.url)}
     end
 
     save_dynarex_index(h)
   end
 
-  def save_title_tags(title, url)
-
-    fields = title.scan(/#(\w+)/)
-    return if fields.empty?
-    
-    if File.exists? @index_filename then
-      dynarex = Dynarex.new @index_filename
-    else
-      dynarex = Dynarex.new('tags/tag(keyword,count)')
-      dynarex.save @index_filename, pretty: true
-    end
-
-    h = dynarex.flat_records.inject({}) do |r,x| 
-      r.merge({x[:keyword] => x[:count].to_i})
-    end
-
-    fields.flatten.uniq.each {|tag| save_tag(h,tag, title, url)}
-    save_dynarex_index(h)
-  end
 
   private
 
   def save_dynarex_index(h)
     
-    dynarex = Dynarex.new('tags/tag(keyword,count)')
-    h.each {|tag,count| dynarex.create keyword: tag, count: count.to_s}
-    dynarex.save @index_filename, pretty: true      
+    dx = Dynarex.new('tags/tag(keyword,count)')
+    h.each {|tag,count| dx.create keyword: tag, count: count.to_s}
+    dx.save @index_filename
   end
 
   def save_tag(h, tag, title, url)
 
-    rec = h[tag]
-    tagfile = tag + '.xml'
-
-    if rec then
-
-      h[tag] += 1
-      dyn = Dynarex.new(tagfile)
-
-    else
-
-      h[tag] = 1
-      dyn = Dynarex.new('items/item(title,url)')
-    end
-
-    dyn.create(url: url, title: title)
-    dyn.save tagfile, pretty: true
+    tagfile = tag + '.xml'    
+    buffer, h[tag] = h[tag] ? [tagfile, h[tag].succ] \
+                                                : ['items/item(title,url)', '1']
+    Dynarex.new(buffer).create(url: url, title: title).save tagfile
   end
   
 end
